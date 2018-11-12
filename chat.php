ca4ff80db0d9d27214c4f555afb32663390f434a
Daiki Shindo <s.daiki.0816@gmail.com>
2018-11-12 15:22:14 +0900

    implimented user name

--- chat.php
+++ chat.php
@@ -7,6 +7,7 @@
 	h1{
 		font-size: 12pt;
 		border-bottom: 1px solid gray;
+		color: blue;
 	}
 	form{
 		border: 1px solid gray;
@@ -24,6 +25,8 @@
 
 <h1>ClosedChatRoom</h1>
 <form action = "write.php">
+	<?php echo $_GET['uname']; ?>
+	<input type = "hidden" name = "uname" value = "<?= $_GET['uname'] ?>">
 	<input type = "text" name = "msg">
 		<button>送信</button>
 	</input>
@@ -33,9 +36,10 @@
 $fp = fopen("data.txt", "r");
 while(($buff = fgets($fp)) != false){
 	$line = explode("\t", $buff);
-	echo $line[0];
-	echo ": ";
-	echo "<span class = \"timestamp\">".date("Y-m-d H:i:s",+- $line[1])."</span>";
+	echo $line[0].":";
+	echo $line[1];
+	echo " ";
+	echo "<span class = \"timestamp\">".date("Y-m-d H:i:s",+- $line[2])."</span>";
 	echo "<br>\n";
 }
 fclose($fp);
