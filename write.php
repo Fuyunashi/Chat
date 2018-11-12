cd42f0669604eb965b46352bcd9cc330e31037c8
Daiki Shindo <s.daiki.0816@gmail.com>
2018-11-12 15:31:24 +0900

    crrection write.php

--- write.php
+++ write.php
@@ -6,7 +6,7 @@ $time = time();
 
 $fp = fopen("data.txt", "a");
 flock($fp, LOCK_EX);
-fwrite($fp, $uname."\t".$msg."\n".$time."\n");
+fwrite($fp, $uname."\n".$msg."\n".$time."\n");
 flock($fp, LOCK_UN);
 fclose($fp);
 
