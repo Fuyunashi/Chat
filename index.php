ef4f0c57e8776212927127520a9a7f4d19d0ce86
Daiki Shindo <s.daiki.0816@gmail.com>
2018-11-12 15:05:18 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+
+<h1>ログイン</h1>
+<form action = "chat.php">
+	<input = type = "text" name = "uname">
+		<button>ログイン</button> 
+	</input>
+</form>
+
+</body>
+</html>
