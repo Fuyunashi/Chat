6b8b3b94c4d04ec92f07dd5dba1021888b086200
Daiki Shindo <s.daiki.0816@gmail.com>
2018-11-26 15:49:55 +0900

    API対応のち、更新

--- get.php
+++ get.php
@@ -1,15 +1,32 @@
 <?php
+require("lib.php");
 $result = [];
 
-$fp = fopen("data.txt", "r");
-while( ($buff=fgets($fp)) != false ){
-	$line = explode("\t", $buff);
-	$result[] = [
-		  "name"    => $line[0]
-		, "message" => $line[1]
-		, "time"    => $line[2]
+/**
+ * MySQLに接続しデータを取得する
+ *
+ */
+// 実行したいSQL
+$sql = 'SELECT * FROM Monster';
+//-------------------------------------------------
+//SQLを実行
+//-------------------------------------------------
+$dbh = connectDB();                 //接続
+$sth = $dbh->prepare($sql);         //SQL準備
+$sth->execute();                    //実行
+//取得した内容を表示する
+while(true){
+    //ここで1レコード取得
+    $buff = $sth->fetch(PDO::FETCH_ASSOC);
+    if( $buff === false ){
+        break;    //データがもう存在しない場合はループを抜ける
+    }
+    // 表示
+    $result[] = [
+		  "name"    => $buff["name"]
+		, "message" => $buff["message"]
+		, "time"    => $buff["time"]
 	];
-}
-fclose($fp);
+};
 
 echo json_encode($result);
---
+++ lib.php
@@ -0,0 +1,7 @@
+<?php
+
+function connectDB(){
+	$dsn  = 'mysql:dbname=chat;host=127.0.0.1';
+	$user = 'root';
+	$pw   = 'H@chiouji1';
+}
